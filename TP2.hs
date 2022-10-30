c90ef96afd9fe4adea6c19f3f1ff1d6638ac9b60
Tomás Bossi <62483708+TomoBossi@users.noreply.github.com>
2022-10-30 19:53:34 -0300

    Update TP2.hs

--- TP2.hs
+++ TP2.hs
@@ -22,6 +22,12 @@ suma z w = (re z + re w, im z + im w)
 producto :: Complejo -> Complejo -> Complejo
 producto z w = (re z * re w - im z * im w, re z * im w + im z * re w)
 
+neg :: Complejo -> Complejo
+neg z = producto z (-1, 0) -- -z
+
+resta :: Complejo -> Complejo -> Complejo
+resta z w = suma z (neg w) -- z - w
+
 -- 1.5
 conjugado :: Complejo -> Complejo
 conjugado z = (re z, -im z)
@@ -29,8 +35,8 @@ conjugado z = (re z, -im z)
 -- 1.6
 inverso :: Complejo -> Complejo
 inverso z = (re conj/mod2, im conj/mod2)
-            where conj = conjugado z
-                  mod2 = (modulo z)^2
+    where conj = conjugado z
+          mod2 = (modulo z)^2
 
 -- 1.7
 cociente :: Complejo -> Complejo -> Complejo
@@ -43,9 +49,11 @@ potencia z k = producto z (potencia z (k-1))
 
 -- 1.9
 raicesCuadratica :: Float -> Float -> Float -> (Complejo, Complejo)
-raicesCuadratica a b c = ((r, d/(2*a)), (r, -d/(2*a)))
-                         where d = sqrt (abs (b^2 - 4*a*c))
-                               r = -b/(2*a)
+raicesCuadratica a b c | d >= 0 = ((r + sd/(2*a), 0), (r - sd/(2*a), 0)) -- raices reales
+                       | otherwise = ((r, sd/(2*a)), (r, -sd/(2*a)))     -- raices complejas
+    where d = b^2 - 4*a*c -- discriminante
+          sd = sqrt (abs d) -- raíz del discriminante
+          r = -b/(2*a)
 
 -- 2.1
 modulo :: Complejo -> Float
@@ -60,29 +68,47 @@ argumento :: Complejo -> Float
 argumento z | a > 0 && b > 0 = atan (b/a)
             | a > 0 && b < 0 = atan (b/a) + 2*pi
             | otherwise = atan (b/a) + pi
-              where a = re z
-                    b = im z
+    where a = re z
+          b = im z
 
 -- 2.4
 pasarACartesianas :: Float -> Float -> Complejo
 pasarACartesianas r a = (r*cos a, r*sin a)
 
 -- 2.5
-raizCuadrada :: Complejo -> (Complejo,Complejo)
+raizCuadrada :: Complejo -> (Complejo, Complejo)
 raizCuadrada z = (w, (-re w, -im w))
-               where w = (x, y)
-                     x = sqrt (abs ((re z + modulo z)/2))
-                     y = s*sqrt (abs ((modulo z - re z)/2))
-                     s = im z/abs (im z) -- signo
+    where w = (r, i)
+          r = sqrt (abs ((re z + modulo z)/2))
+          i = s*sqrt (abs ((modulo z - re z)/2))
+          s | im z == 0 = 1
+            | otherwise = im z/abs (im z) -- signo
 
 -- 2.6
--- raicesCuadraticaCompleja :: Complejo -> Complejo -> Complejo -> (Complejo,Complejo)
--- raicesCuadraticaCompleja a b c = 
+raicesCuadraticaCompleja :: Complejo -> Complejo -> Complejo -> (Complejo, Complejo)
+raicesCuadraticaCompleja a b c = (w1, w2)
+    where w1 = cociente (suma  (neg b) sd) (producto (2, 0) a)
+          w2 = cociente (resta (neg b) sd) (producto (2, 0) a)
+          sd = fst (raizCuadrada (resta (potencia b 2) ac4)) -- raíz del discriminante
+          ac4 = producto (4, 0) (producto a c) -- 4ac
 
 -- 3.1
--- raicesNEsimas :: Integer -> [Complejo]
--- raicesNEsimas n = -- Probablemente requiera aux raicesNEsimasDesde n m con caso base m == 0 para siempre poder sumar ángulo dividido por n
+reverso :: [Complejo] -> [Complejo]
+reverso xs = reversoAux xs []
+    where reversoAux :: [Complejo] -> [Complejo] -> [Complejo]
+          reversoAux [] sxs = sxs
+          reversoAux (x:xs) sxs = reversoAux xs (x:sxs)
+
+raicesNEsimas :: Integer -> [Complejo]
+raicesNEsimas n = reverso (raicesNEsimasDesde n n)
+    where raicesNEsimasDesde :: Integer -> Integer -> [Complejo]
+          raicesNEsimasDesde n 1 = [(1,0)] -- [w0]
+          raicesNEsimasDesde n m = producto w1 (head wp):wp
+              where w1 = pasarACartesianas 1 (2*pi/(fromIntegral n))
+                    wp = raicesNEsimasDesde n (m-1)
 
 -- 3.2
--- sonRaicesNEsimas :: Integer -> [Complejo] -> Float -> [Bool]
--- sonRaicesNEsimas n rs eps = 
+sonRaicesNEsimas :: Integer -> [Complejo] -> Float -> Bool
+sonRaicesNEsimas n [] tol = True
+sonRaicesNEsimas n rs tol = error < tol && sonRaicesNEsimas n (tail rs) tol
+    where error = modulo (resta (potencia (head rs) n) (1,0))
