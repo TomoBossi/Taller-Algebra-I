dfcd1d942ff101a4ed1fccafa6ff4a7091ef976b
Tomás Bossi <62483708+TomoBossi@users.noreply.github.com>
2022-11-01 09:44:31 -0300

    Update clase07.hs

--- clase07.hs
+++ clase07.hs
@@ -12,3 +12,17 @@ incluido (x:xs) b = elem x b && incluido xs b
 
 iguales :: Set Int -> Set Int -> Bool
 iguales a b = incluido a b && incluido b a
+
+partes :: Int -> Set (Set Int)
+partes 0 = [[]]
+partes n = agregarTodos n (partes (n-1)) ++ partes (n-1)
+    where agregarTodos :: Int -> Set [Int] -> Set [Int]
+          agregarTodos _ [] = []
+          agregarTodos x (c:cs) = [x:c] ++ agregarTodos x cs
+
+productoCartesiano :: Set Int -> Set Int -> Set (Int,Int)
+productoCartesiano [] _ = []
+productoCartesiano (x:xs) ys = emparejar x ys ++ productoCartesiano xs ys
+    where emparejar :: Int -> Set Int -> Set (Int,Int)
+          emparejar x [] = []
+          emparejar x (y:ys) = (x,y):emparejar x ys
