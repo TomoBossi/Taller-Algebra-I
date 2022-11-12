92f2b0bb11017850a34cbfb3e1344899fc5193ff
Tomás Bossi <62483708+TomoBossi@users.noreply.github.com>
2022-11-12 13:01:29 -0300

    Update TP2.hs

--- TP2.hs
+++ TP2.hs
@@ -1,114 +1,99 @@
--- 
--- 
--- 
-
--- TP 2
+-- Bossi Tomás tomasbossi97@gmail.com
+-- González Gastón gasti.g.gg@gmail.com
+-- Muñoz Sandra sm9654136@gmail.com
 
 type Complejo = (Float,Float)
 
+-- Auxiliares
+prodPorEscalar :: Float -> Complejo -> Complejo
+prodPorEscalar k (a, b) = (k*a, k*b)
+
 -- 1.1
 re :: Complejo -> Float
-re z = fst z
+re (a, _) = a
 
 -- 1.2
 im :: Complejo -> Float
-im z = snd z
+im (_, b) = b
 
 -- 1.3
 suma :: Complejo -> Complejo -> Complejo
-suma z w = (re z + re w, im z + im w)
+suma (a1, b1) (a2, b2) = (a1 + a2, b1 + b2)
 
 -- 1.4
 producto :: Complejo -> Complejo -> Complejo
-producto z w = (re z * re w - im z * im w, re z * im w + im z * re w)
-
-neg :: Complejo -> Complejo
-neg z = producto z (-1, 0) -- -z
-
-resta :: Complejo -> Complejo -> Complejo
-resta z w = suma z (neg w) -- z - w
+producto (a1, b1) (a2, b2) = (a1*a2 - b1*b2, a1*b2 + a2*b1)
 
 -- 1.5
 conjugado :: Complejo -> Complejo
-conjugado z = (re z, -im z)
+conjugado (a, b) = (a, -b)
 
 -- 1.6
 inverso :: Complejo -> Complejo
-inverso z = (re conj/mod2, im conj/mod2)
-    where conj = conjugado z
-          mod2 = (modulo z)^2
+inverso (a, b) = (a/m, -b/m)
+               where m = a**2 + b**2
 
 -- 1.7
 cociente :: Complejo -> Complejo -> Complejo
-cociente z w = producto z (inverso w)
+cociente z1 z2 = producto z1 (inverso z2)
 
 -- 1.8
 potencia :: Complejo -> Integer -> Complejo
-potencia z 1 = z
-potencia z k = producto z (potencia z (k-1))
+potencia z 0 = (1, 0)
+potencia z k = producto z (potencia z (k - 1))
 
 -- 1.9
 raicesCuadratica :: Float -> Float -> Float -> (Complejo,Complejo)
-raicesCuadratica a b c | d >= 0 = ((r + sd/(2*a), 0), (r - sd/(2*a), 0)) -- raíces reales
-                       | otherwise = ((r, sd/(2*a)), (r, -sd/(2*a)))     -- races complejas
-    where d = b^2 - 4*a*c -- discriminante
-          sd = sqrt (abs d) -- raíz del discriminante
-          r = -b/(2*a)
+raicesCuadratica a b c
+  | disc < 0 = (w, conjugado w)
+  | otherwise = (((fst w + snd w)/(2*a), 0), ((fst w - snd w)/(2*a), 0))
+  where
+    disc = b**2 - 4*a*c
+    w = prodPorEscalar (1/(2*a)) (-b, (sqrt (abs disc)))
 
 -- 2.1
 modulo :: Complejo -> Float
-modulo z = sqrt ((re z)^2 + (im z)^2)
+modulo (a, b) = sqrt (a**2 + b**2)
 
 -- 2.2
 distancia :: Complejo -> Complejo -> Float
-distancia z w = sqrt ((re z - re w)^2 + (im z - im w)^2)
+distancia (a1, b1) (a2, b2) = modulo (a1 - a2, b1 - b2)
 
 -- 2.3
 argumento :: Complejo -> Float
-argumento z | a > 0 && b > 0 = atan (b/a)
-            | a > 0 && b < 0 = atan (b/a) + 2*pi
-            | otherwise = atan (b/a) + pi
-    where a = re z
-          b = im z
+argumento (a, 0) = (pi - pi*(a/(abs a)))/2
+argumento (0, b) = pi/2 + (pi - pi*(b/(abs b)))/2
+argumento (a, b) = atan (b/a) + (pi - pi*(a*b)/(abs (a*b)))/2 + (pi - pi*b/(abs b))/2
 
 -- 2.4
 pasarACartesianas :: Float -> Float -> Complejo
-pasarACartesianas r a = (r*cos a, r*sin a)
+pasarACartesianas r t = (r*cos t, r*sin t)
 
 -- 2.5
 raizCuadrada :: Complejo -> (Complejo,Complejo)
-raizCuadrada z = (w, neg w)
-    where w = (r, i)
-          r = sqrt (abs ((re z + modulo z)/2))
-          i = s*sqrt (abs ((modulo z - re z)/2))
-          s | im z == 0 = 1
-            | otherwise = im z/abs (im z) -- signo
+raizCuadrada z = (w, prodPorEscalar (-1) w)
+  where 
+    t = argumento z
+    w = prodPorEscalar (sqrt (modulo z)) (cos (t/2), sin (t/2))
 
 -- 2.6
 raicesCuadraticaCompleja :: Complejo -> Complejo -> Complejo -> (Complejo,Complejo)
-raicesCuadraticaCompleja a b c = (w1, w2)
-    where w1 = cociente (suma  (neg b) sd) (producto (2, 0) a)
-          w2 = cociente (resta (neg b) sd) (producto (2, 0) a)
-          sd = fst (raizCuadrada (resta (potencia b 2) ac4)) -- raíz del "discriminante" w
-          ac4 = producto (4, 0) (producto a c) -- 4ac
+raicesCuadraticaCompleja a b c = (producto (suma (prodPorEscalar (-1) b) (fst w)) (inverso (prodPorEscalar 2 a)), producto (suma (prodPorEscalar (-1) b) (snd w)) (inverso (prodPorEscalar 2 a)))
+  where
+    w = raizCuadrada (suma (potencia b 2) (prodPorEscalar (-4) (producto a c)))
 
 -- 3.1
-reverso :: [Complejo] -> [Complejo]
-reverso xs = reversoAux xs []
-    where reversoAux :: [Complejo] -> [Complejo] -> [Complejo]
-          reversoAux [] sxs = sxs
-          reversoAux (x:xs) sxs = reversoAux xs (x:sxs)
-
 raicesNEsimas :: Integer -> [Complejo]
-raicesNEsimas n = reverso (raicesNEsimasDesde n n)
-    where raicesNEsimasDesde :: Integer -> Integer -> [Complejo]
-          raicesNEsimasDesde n 1 = [(1,0)] -- [w0]
-          raicesNEsimasDesde n m = producto w1 (head wp):wp
-              where w1 = pasarACartesianas 1 (2*pi/(fromIntegral n))
-                    wp = raicesNEsimasDesde n (m-1)
+raicesNEsimas n = gn n 0
+  where
+    gn :: Integer -> Integer -> [Complejo]
+    gn n k
+      | k >= n = []
+      | otherwise = (cos t, sin t) : gn n (k + 1)
+      where
+        t = (2*(fromIntegral k)*pi/(fromIntegral n))
 
 -- 3.2
 sonRaicesNEsimas :: Integer -> [Complejo] -> Float -> Bool
-sonRaicesNEsimas n [] tol = True
-sonRaicesNEsimas n (w:ws) tol = error < tol && sonRaicesNEsimas n ws tol
-    where error = modulo (resta (potencia w n) (1,0))
+sonRaicesNEsimas _ [] _ = True
+sonRaicesNEsimas n (z:zs) tol = modulo (suma (potencia z n) (-1, 0)) < tol && sonRaicesNEsimas n zs tol
